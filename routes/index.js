2a768197ac476c917acc7fafe21b23194ee59c98
Khai Pham <minhkhai3012@gmail.com>
2019-04-16 22:28:38 +0700

    create user page, product page, home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Trang chủ' });
 });
+
+/*GET User page. */
+router.get('/user', (req, res, next) => {
+  res.render('user', {title: 'Quản lý người sử dụng'});
+});
+
+/*GET User-detail page. */
+router.get('/user_detail', (req, res, next) => {
+  res.render('user_detail', {title: 'Cập nhật thông tin'});
+});
+
+/*GET Product page. */
+router.get('/product', (req, res, next) => {
+  res.render('product', {title: 'Sản phẩm'});
+});
+
 
 module.exports = router;
