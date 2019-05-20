76c170e8130a8c9db3408a11a9ba934e796538c5
Khai Pham <minhkhai3012@gmail.com>
2019-05-20 21:32:26 +0700

    create env

--- dbs/index.js
+++ dbs/index.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
-const mongo = require('mongodb')
 // connection string
-const PROD_URI = "mongodb+srv://khaipham:123456789a@cluster0-lwsla.mongodb.net/LTW-data?retryWrites=true";
+//const PROD_URI = "mongodb+srv://khaipham:123456789a@cluster0-lwsla.mongodb.net/LTW-data?retryWrites=true";
+const PROD_URI = process.env.PROD_DB_URI || "mongodb://127.0.0.1:27017/data";
 
 var dbs = {
     production: {}
