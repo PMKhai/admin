c9c79835965c6b504ab70540dc473ed9c225630a
Khai Pham <minhkhai3012@gmail.com>
2019-06-15 20:00:25 +0700

    load order list.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,17 +1,52 @@
-const customer = require('../models/customer');
+const customer = require("../models/customer");
 
 exports.orders_list = async (req, res, next) => {
-    if (!req.user)
-        res.redirect('/authen');
-    if (req.user.role !== "admin")
-        var role = {};
-    const customerList = await customer.listAll();
-    let order = [];
+  if (!req.user) res.redirect("/authen");
+  if (req.user.role !== "admin") var role = {};
 
-    res.render('order/index', {
-        title: 'Đơn đặt hàng',
-        admin: req.user,
-        role
-    });
-    x
+  const customerList = await customer.listAll();
+  let order = [];
+  customerList.forEach(element => {
+    if (element.listorder != undefined || element.listorder != null) {
+      order.push(element.listorder);
+    }
+  });
+
+  for (let i = 0; i < order.length; i++) {
+    let size = order[i].length;
+    for (let y = 0; y < size; y++) {
+      order[i][y].index = i;
+
+      if (order[i][y].status === "đang giao")
+        order[i][y].display = "btn-warning"
+      if (order[i][y].status === "đã giao")
+        order[i][y].display = "btn-success"
+      if (order[i][y].status === "hủy đơn")
+        order[i][y].display = "btn-danger"
+
+    }
+  }
+
+  res.render("order/index", {
+    title: "Đơn đặt hàng",
+    admin: req.user,
+    role,
+    order
+  });
+  x;
+};
+
+exports.orderDetail = async (req, res, next) => {
+  if (!req.user) res.redirect("/authen");
+  if (req.user.role !== "admin") var role = {};
+
+  const customerList = await customer.listAll();
+  let order = [];
+  customerList.forEach(element => {
+    if (element.listorder != undefined || element.listorder != null) {
+      order.push(element.listorder);
+    }
+  });
+  const orderDetail = order[req.params.indexInUserlist][req.params.indexInOrderlist];
+
 }
--- routes/orders.js
+++ routes/orders.js
@@ -6,4 +6,7 @@ var order_controller = require('../controllers/orderController');
 /* GET order listing*/
 router.get('/', order_controller.orders_list);
 
+/*Get order detail. */
+router.get('/detail/:indexInUserlist/:indexInOrderlist', order_controller.orderDetail);
+
 module.exports = router;
